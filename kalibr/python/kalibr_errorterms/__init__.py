16aa37eaab8183a20b5b6f9f7225347d9f20f01d
Guoxiang Zhou <gzhou@anybotics.com>
2020-09-21 15:55:25 +0000

    Merge branch 'fix/wac_ipqc' into 'master'
    [IPQC WAC Intrinsic Calibration] Add ros version detection.
    
    GitOrigin-RevId: ea99b31f68e79af1f3073b1b3174e4ba9bc35375

--- kalibr/python/kalibr_errorterms/__init__.py
+++ kalibr/python/kalibr_errorterms/__init__.py
@@ -1,2 +1,4 @@
 import aslam_cv_backend_python #needed for errorterm base class wrapper
-from libkalibr_errorterms_python import *
+# This works when you install the debian package ros-noetic-kalibr. If you build it locally, you need to use from libkalibr_errorterms_python import * instead.
+# Todo: need to make it consistent in both locally build and debian.
+from kalibr.libkalibr_errorterms_python import *
